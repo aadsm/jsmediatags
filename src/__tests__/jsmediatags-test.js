3d45a728bb57dbeda727011e4056fa80122873a1
António Afonso <antonio.afonso@gmail.com>
2015-11-29 12:15:35 -0800

    Add jsmediatags
    This will be the public API of the library:
    
    // simpler version
    jsmediatags.read(path, {onSuccess, onError});
    
    // customizable version
    new jsmediatags.Reader(path)
    	.setFileReader(NodeFileReader)
    	.read({onSuccess, onError});

---
+++ src/__tests__/jsmediatags-test.js
@@ -0,0 +1,132 @@
+jest
+  .dontMock("../jsmediatags.js")
+  .dontMock("../ByteArrayUtils.js");
+
+const jsmediatags = require("../jsmediatags");
+const NodeFileReader = require("../NodeFileReader");
+const XhrFileReader = require("../XhrFileReader");
+const ID3v2TagReader = require("../ID3v2TagReader");
+const bin = require("../ByteArrayUtils").bin;
+
+describe("jsmediatags", function() {
+  var mockFileReader;
+  var mockTags = {};
+
+  beforeEach(function() {
+    // Reset auto mock to its original state.
+    NodeFileReader.canReadFile = jest.genMockFunction();
+    NodeFileReader.prototype.loadRange = jest.genMockFunction()
+      .mockImplementation(function(range, callbacks) {
+        setTimeout(function() {
+          callbacks.onSuccess();
+        }, 1);
+      });
+
+    ID3v2TagReader.getTagIdentifierByteRange.mockReturnValue([]);
+    ID3v2TagReader.prototype.setTags = jest.genMockFunction().mockReturnThis();
+  });
+
+  pit("should read tags with the shortcut function", function() {
+    NodeFileReader.canReadFile.mockReturnValue(true);
+    ID3v2TagReader.canReadTagFormat.mockReturnValue(true);
+    ID3v2TagReader.prototype.read = jest.genMockFunction()
+      .mockImplementation(function(callbacks) {
+        callbacks.onSuccess(mockTags);
+      });
+
+    return new Promise(function(resolve, reject) {
+      jsmediatags.read("fakefile", {onSuccess: resolve, onError: reject});
+      jest.runAllTimers();
+    }).then(function(tags) {
+      expect(tags).toBe(mockTags);
+    });
+  });
+
+  describe("file readers", function() {
+    it("should use the given file reader", function() {
+      var reader = new jsmediatags.Reader();
+      var MockFileReader = jest.genMockFunction();
+
+      reader.setFileReader(MockFileReader);
+      var fileReader = reader._getFileReader();
+
+      expect(fileReader).toBe(MockFileReader);
+    });
+
+    it("should use the node file reader", function() {
+      NodeFileReader.canReadFile.mockReturnValue(true);
+
+      var reader = new jsmediatags.Reader();
+      var FileReader = reader._getFileReader();
+
+      expect(FileReader).toBe(NodeFileReader);
+    });
+
+    it("should use the XHR file reader", function() {
+      XhrFileReader.canReadFile.mockReturnValue(true);
+
+      var reader = new jsmediatags.Reader();
+      var FileReader = reader._getFileReader();
+
+      expect(FileReader).toBe(XhrFileReader);
+    });
+  });
+
+  describe("tag readers", function() {
+    pit("should use the given tag reader", function() {
+      var MockTagReader = jest.genMockFunction();
+
+      return new Promise(function(resolve, reject) {
+        var reader = new jsmediatags.Reader();
+        reader.setTagReader(MockTagReader);
+        reader._getTagReader(null, {onSuccess: resolve, onError: reject});
+        jest.runAllTimers();
+      }).then(function(TagReader) {
+        expect(TagReader).toBe(MockTagReader);
+      });
+    });
+
+    pit("should use the tag reader that is able to read the tags", function() {
+      var MockTagReader = jest.genMockFunction();
+      jsmediatags.Config.addTagReader(MockTagReader);
+
+      ID3v2TagReader.canReadTagFormat.mockReturnValue(false);
+      MockTagReader.getTagIdentifierByteRange = jest.genMockFunction()
+        .mockReturnValue([]);
+      MockTagReader.canReadTagFormat = jest.genMockFunction()
+        .mockReturnValue(true);
+
+      return new Promise(function(resolve, reject) {
+        var reader = new jsmediatags.Reader();
+        reader._getTagReader(new NodeFileReader(), {onSuccess: resolve, onError: reject});
+        jest.runAllTimers();
+      }).then(function(TagReader) {
+        jsmediatags.Config.removeTagReader(MockTagReader);
+
+        expect(TagReader).toBe(MockTagReader);
+      });
+    });
+
+    pit("should load the super set range of all tag reader ranges", function() {
+      var MockTagReader = jest.genMockFunction();
+      jsmediatags.Config.addTagReader(MockTagReader);
+
+      ID3v2TagReader.canReadTagFormat.mockReturnValue(false);
+      ID3v2TagReader.getTagIdentifierByteRange.mockReturnValue([2, 4]);
+      MockTagReader.getTagIdentifierByteRange = jest.genMockFunction()
+        .mockReturnValue([5, 6]);
+      MockTagReader.canReadTagFormat = jest.genMockFunction()
+        .mockReturnValue(true);
+
+      return new Promise(function(resolve, reject) {
+        var reader = new jsmediatags.Reader();
+        reader._findTagReader(new NodeFileReader(), {onSuccess: resolve, onError: reject});
+        jest.runAllTimers();
+      }).then(function() {
+        jsmediatags.Config.removeTagReader(MockTagReader);
+        var rangeSuperset = NodeFileReader.prototype.loadRange.mock.calls[0][0];
+        expect(rangeSuperset).toEqual([2, 6]);
+      });
+    });
+  });
+});
---
+++ src/jsmediatags.js
@@ -0,0 +1,167 @@
+/**
+ * @flow
+ */
+'use strict';
+
+const MediaFileReader = require("./MediaFileReader");
+const NodeFileReader = require("./NodeFileReader");
+const XhrFileReader = require("./XhrFileReader");
+const MediaTagReader = require("./MediaTagReader");
+const ID3v2TagReader = require("./ID3v2TagReader");
+
+import type {
+  CallbackType,
+  LoadCallbackType
+} from './FlowTypes';
+
+var mediaFileReaders: Array<Class<MediaFileReader>> = [];
+var mediaTagReaders: Array<Class<MediaTagReader>> = [];
+
+function read(location: Object, callbacks: CallbackType) {
+  new Reader(location).read(callbacks);
+}
+
+class Reader {
+  _file: any;
+  _tags: Array<string>;
+  _fileReader: Class<MediaFileReader>;
+  _tagReader: Class<MediaTagReader>;
+
+  constructor(file: any) {
+    this._file = file;
+  }
+
+  setTags(tags: Array<string>): Reader {
+    this._tags = tags;
+    return this;
+  }
+
+  setFileReader(fileReader: Class<MediaFileReader>): Reader {
+    this._fileReader = fileReader;
+    return this;
+  }
+
+  setTagReader(tagReader: Class<MediaTagReader>): Reader {
+    this._tagReader = tagReader;
+    return this;
+  }
+
+  read(callbacks: CallbackType) {
+    var FileReader = this._getFileReader();
+    var fileReader = new FileReader(this._file);
+    var self = this;
+
+    this._getTagReader(fileReader, {
+      onSuccess: function(TagReader: Class<MediaTagReader>) {
+        new TagReader(fileReader)
+          .setTags(self._tags)
+          .read(callbacks);
+      },
+      onError: callbacks.onError
+    });
+  }
+
+  _getFileReader(): Class<MediaFileReader> {
+    if (this._fileReader) {
+      return this._fileReader;
+    } else {
+      return this._findFileReader();
+    }
+  }
+
+  _findFileReader(): Class<MediaFileReader> {
+    for (var i = 0; i < mediaFileReaders.length; i++) {
+      if (mediaFileReaders[i].canReadFile(this._file)) {
+        return mediaFileReaders[i];
+      }
+    }
+
+    throw new Error("No suitable file reader found for ", this._file);
+  }
+
+  _getTagReader(fileReader: MediaFileReader, callbacks: CallbackType) {
+    if (this._tagReader) {
+      var tagReader = this._tagReader;
+      setTimeout(function() {
+        callbacks.onSuccess(tagReader);
+      }, 1);
+    } else {
+      this._findTagReader(fileReader, callbacks);
+    }
+  }
+
+  _findTagReader(fileReader: MediaFileReader, callbacks: CallbackType) {
+    var tagIdentifierRange = [Number.MAX_VALUE, 0];
+
+    // Create a super set of all ranges so we can load them all at once.
+    // Might need to rethink this approach if there are tag ranges too far
+    // a part from each other. We're good for now though.
+    for (var i = 0; i < mediaTagReaders.length; i++) {
+      var range = mediaTagReaders[i].getTagIdentifierByteRange();
+
+      if (range[1] < range[0]) {
+        throw new Error("Invalid range: ", range, " from ", mediaTagReaders[i]);
+      }
+
+      tagIdentifierRange[0] = Math.min(range[0], tagIdentifierRange[0]);
+      tagIdentifierRange[1] = Math.max(range[1], tagIdentifierRange[1]);
+    }
+
+    fileReader.loadRange(tagIdentifierRange, {
+      onSuccess: function() {
+        for (var i = 0; i < mediaTagReaders.length; i++) {
+          var range = mediaTagReaders[i].getTagIdentifierByteRange();
+          var tagIndentifier = fileReader.getBytesAt(
+            range[0],
+            range[1] - range[0] + 1
+          );
+          if (mediaTagReaders[i].canReadTagFormat(tagIndentifier)) {
+            callbacks.onSuccess(mediaTagReaders[i]);
+            return;
+          }
+        }
+      },
+      onError: callbacks.onError
+    });
+  }
+}
+
+class Config {
+  static addFileReader(fileReader: Class<MediaFileReader>): Class<Config> {
+    mediaFileReaders.push(fileReader);
+    return this;
+  }
+
+  static addTagReader(tagReader: Class<MediaTagReader>): Class<Config> {
+    mediaTagReaders.push(tagReader);
+    return this;
+  }
+
+  static removeTagReader(tagReader: Class<MediaTagReader>): Class<Config> {
+    var tagReaderIx = mediaTagReaders.indexOf(tagReader);
+
+    if (tagReaderIx >= 0) {
+      mediaTagReaders.splice(tagReaderIx, 1);
+    }
+
+    return this;
+  }
+}
+
+Config
+  // $FlowIssue - flow doesn't allow type to pass as their supertype
+  .addFileReader(XhrFileReader)
+  // $FlowIssue - flow doesn't allow type to pass as their supertype
+  .addTagReader(ID3v2TagReader);
+
+if (typeof process !== "undefined") {
+  Config
+    // $FlowIssue - flow doesn't allow type to pass as their supertype
+    .addFileReader(NodeFileReader);
+}
+
+module.exports = {
+  "read": read,
+  "Reader": Reader,
+  "Config": Config
+};

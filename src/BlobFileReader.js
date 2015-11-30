c8e69eba183f8287bfe0eb81684965d379970fdf
António Afonso <antonio.afonso@gmail.com>
2015-11-30 00:00:52 -0800

    Add BlobFileReader

---
+++ src/BlobFileReader.js
@@ -0,0 +1,64 @@
+/**
+ * @flow
+ */
+'use strict';
+
+const ChunkedFileData = require('./ChunkedFileData');
+const MediaFileReader = require('./MediaFileReader');
+
+import type {
+  LoadCallbackType
+} from './FlowTypes';
+
+class BlobFileReader extends MediaFileReader {
+  _blob: Blob;
+  // $FlowIssue - Flow gets confused with module.exports
+  _fileData: ChunkedFileData;
+
+  constructor(blob: Blob) {
+    super();
+    this._blob = blob;
+    // $FlowIssue - Constructor cannot be called on exports
+    this._fileData = new ChunkedFileData();
+  }
+
+  static canReadFile(file: any): boolean {
+    return (
+      typeof Blob !== "undefined" &&
+      file instanceof Blob
+    );
+  }
+
+  _init(callbacks: LoadCallbackType): void {
+    this._size = this._blob.size;
+    setTimeout(callbacks.onSuccess, 1);
+  }
+
+  loadRange(range: [number, number], callbacks: LoadCallbackType): void {
+    var self = this;
+    // $FlowIssue - flow isn't aware of mozSlice or webkitSlice
+    var blobSlice = this._blob.slice || this._blob.mozSlice || this._blob.webkitSlice;
+    var blob = blobSlice.call(this._blob, range[0], range[1] + 1);
+    var browserFileReader = new FileReader();
+
+    browserFileReader.onloadend = function(event) {
+      var intArray = new Uint8Array(browserFileReader.result);
+      self._fileData.addData(range[0], intArray);
+      callbacks.onSuccess();
+    };
+    browserFileReader.onerror =
+    browserFileReader.onabort = function(event) {
+      if (callbacks.onError) {
+        callbacks.onError({"type": "blob", "blob": browserFileReader.error});
+      }
+    };
+
+    browserFileReader.readAsArrayBuffer(blob);
+  }
+
+  getByteAt(offset: number): number {
+    return this._fileData.getByteAt(offset);
+  }
+}
+
+module.exports = BlobFileReader;
---
+++ src/__tests__/BlobFileReader-test.js
@@ -0,0 +1,77 @@
+jest
+  .dontMock('../BlobFileReader.js')
+  .dontMock('../MediaFileReader.js')
+  .dontMock('../ChunkedFileData.js');
+
+var BlobFileReader = require('../BlobFileReader');
+
+function throwOnError(onSuccess) {
+  return {
+    onSuccess: onSuccess,
+    onError: function() {
+      throw new Error();
+    }
+  }
+}
+
+function throwOnSuccess(onError) {
+  return {
+    onSuccess: function() {
+      throw new Error();
+    },
+    onError: onError
+  }
+}
+
+describe("BlobFileReader", function() {
+  var fileReader;
+
+  beforeEach(function() {
+    fileReader = new BlobFileReader(new Blob(["This is a simple file"]));
+  });
+
+  it("should be able to read the right type of files", function() {
+    expect(BlobFileReader.canReadFile("fakefile")).toBe(false);
+    expect(BlobFileReader.canReadFile("http://localhost")).toBe(false);
+    expect(BlobFileReader.canReadFile(new Blob())).toBe(true);
+  });
+
+  pit("should have the right size information", function() {
+    return new Promise(function(resolve, reject) {
+      fileReader.init(throwOnError(resolve));
+      jest.runAllTimers();
+    }).then(function() {
+      expect(fileReader.getSize()).toBe(21);
+    });
+  });
+
+  pit("should read a byte", function() {
+    return new Promise(function(resolve, reject) {
+      fileReader.loadRange([0, 4], throwOnError(resolve));
+      jest.runAllTimers();
+    }).then(function(tags) {
+      expect(fileReader.getByteAt(0)).toBe("T".charCodeAt(0));
+    });
+  });
+
+  pit("should read a byte after loading the same range twice", function() {
+    return new Promise(function(resolve, reject) {
+      fileReader.loadRange([0, 4], throwOnError(function() {
+        fileReader.loadRange([0, 4], throwOnError(resolve));
+      }));
+    }).then(function(tags) {
+      expect(fileReader.getByteAt(0)).toBe("T".charCodeAt(0));
+    });
+  });
+
+  pit("should not read a byte that hasn't been loaded yet", function() {
+    return new Promise(function(resolve, reject) {
+      fileReader.init(throwOnError(resolve));
+      jest.runAllTimers();
+    }).then(function(tags) {
+      expect(function() {
+        var byte0 = fileReader.getByteAt(0);
+      }).toThrow();
+    });
+  });
+});

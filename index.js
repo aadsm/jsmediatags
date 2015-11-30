56309f36552a0e56a7342d77a78f59005f55dd20
António Afonso <antonio.afonso@gmail.com>
2015-11-30 00:12:33 -0800

    Remove unused file

--- index.js
+++
@@ -1,7 +0,0 @@
-var jsmediatags = {
-  NodeFileReader: require('./build/NodeFileReader'),
-  XhrFileReader: require('./build/XhrFileReader'),
-  ID3v2TagReader:  require('./build/ID3v2TagReader')
-};
-
-module.exports = jsmediatags;

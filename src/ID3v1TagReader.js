b664f9d7136043f7bea7287590a7c35474b0a6dd
António Afonso <antonio.afonso@gmail.com>
2015-12-27 20:05:45 -0800

    Fix ID3v1TagReader indentation

--- src/ID3v1TagReader.js
+++ src/ID3v1TagReader.js
@@ -36,38 +36,38 @@ class ID3v1TagReader extends MediaTagReader {
   _parseData(data: MediaFileReader, tags: ?Array<string>): ID3v1TagType {
     var offset = data.getSize() - 128;
 
-		var title = data.getStringWithCharsetAt(offset + 3, 30).toString();
-		var artist = data.getStringWithCharsetAt(offset + 33, 30).toString();
-		var album = data.getStringWithCharsetAt(offset + 63, 30).toString();
-		var year = data.getStringWithCharsetAt(offset + 93, 4).toString();
+    var title = data.getStringWithCharsetAt(offset + 3, 30).toString();
+    var artist = data.getStringWithCharsetAt(offset + 33, 30).toString();
+    var album = data.getStringWithCharsetAt(offset + 63, 30).toString();
+    var year = data.getStringWithCharsetAt(offset + 93, 4).toString();
 
-		var trackFlag = data.getByteAt(offset + 97 + 28);
+    var trackFlag = data.getByteAt(offset + 97 + 28);
     var track = data.getByteAt(offset + 97 + 29);
-		if (trackFlag == 0 && track != 0) {
+    if (trackFlag == 0 && track != 0) {
       var version = "1.1";
-			var comment = data.getStringWithCharsetAt(offset + 97, 28).toString();
-		} else {
+      var comment = data.getStringWithCharsetAt(offset + 97, 28).toString();
+    } else {
       var version = "1.0";
-			var comment = data.getStringWithCharsetAt(offset + 97, 30).toString();
-			track = 0;
-		}
+      var comment = data.getStringWithCharsetAt(offset + 97, 30).toString();
+      track = 0;
+    }
 
-		var genreIdx = data.getByteAt(offset + 97 + 30);
-		if (genreIdx < 255) {
-			var genre = GENRES[genreIdx];
-		} else {
-			var genre = "";
-		}
+    var genreIdx = data.getByteAt(offset + 97 + 30);
+    if (genreIdx < 255) {
+      var genre = GENRES[genreIdx];
+    } else {
+      var genre = "";
+    }
 
-		var tag = {
-		  "version" : version,
-			"title" : title,
-			"artist" : artist,
-			"album" : album,
-			"year" : year,
-			"comment" : comment,
-			"genre" : genre
-		};
+    var tag = {
+      "version" : version,
+      "title" : title,
+      "artist" : artist,
+      "album" : album,
+      "year" : year,
+      "comment" : comment,
+      "genre" : genre
+    };
 
     if (track) {
       // $FlowIssue - flow is not happy with adding properties even when they're optional.
@@ -79,28 +79,28 @@ class ID3v1TagReader extends MediaTagReader {
 }
 
 var GENRES = [
-	"Blues","Classic Rock","Country","Dance","Disco","Funk","Grunge",
-	"Hip-Hop","Jazz","Metal","New Age","Oldies","Other","Pop","R&B",
-	"Rap","Reggae","Rock","Techno","Industrial","Alternative","Ska",
-	"Death Metal","Pranks","Soundtrack","Euro-Techno","Ambient",
-	"Trip-Hop","Vocal","Jazz+Funk","Fusion","Trance","Classical",
-	"Instrumental","Acid","House","Game","Sound Clip","Gospel",
-	"Noise","AlternRock","Bass","Soul","Punk","Space","Meditative",
-	"Instrumental Pop","Instrumental Rock","Ethnic","Gothic",
-	"Darkwave","Techno-Industrial","Electronic","Pop-Folk",
-	"Eurodance","Dream","Southern Rock","Comedy","Cult","Gangsta",
-	"Top 40","Christian Rap","Pop/Funk","Jungle","Native American",
-	"Cabaret","New Wave","Psychadelic","Rave","Showtunes","Trailer",
-	"Lo-Fi","Tribal","Acid Punk","Acid Jazz","Polka","Retro",
-	"Musical","Rock & Roll","Hard Rock","Folk","Folk-Rock",
-	"National Folk","Swing","Fast Fusion","Bebob","Latin","Revival",
-	"Celtic","Bluegrass","Avantgarde","Gothic Rock","Progressive Rock",
-	"Psychedelic Rock","Symphonic Rock","Slow Rock","Big Band",
-	"Chorus","Easy Listening","Acoustic","Humour","Speech","Chanson",
-	"Opera","Chamber Music","Sonata","Symphony","Booty Bass","Primus",
-	"Porn Groove","Satire","Slow Jam","Club","Tango","Samba",
-	"Folklore","Ballad","Power Ballad","Rhythmic Soul","Freestyle",
-	"Duet","Punk Rock","Drum Solo","Acapella","Euro-House","Dance Hall"
+  "Blues","Classic Rock","Country","Dance","Disco","Funk","Grunge",
+  "Hip-Hop","Jazz","Metal","New Age","Oldies","Other","Pop","R&B",
+  "Rap","Reggae","Rock","Techno","Industrial","Alternative","Ska",
+  "Death Metal","Pranks","Soundtrack","Euro-Techno","Ambient",
+  "Trip-Hop","Vocal","Jazz+Funk","Fusion","Trance","Classical",
+  "Instrumental","Acid","House","Game","Sound Clip","Gospel",
+  "Noise","AlternRock","Bass","Soul","Punk","Space","Meditative",
+  "Instrumental Pop","Instrumental Rock","Ethnic","Gothic",
+  "Darkwave","Techno-Industrial","Electronic","Pop-Folk",
+  "Eurodance","Dream","Southern Rock","Comedy","Cult","Gangsta",
+  "Top 40","Christian Rap","Pop/Funk","Jungle","Native American",
+  "Cabaret","New Wave","Psychadelic","Rave","Showtunes","Trailer",
+  "Lo-Fi","Tribal","Acid Punk","Acid Jazz","Polka","Retro",
+  "Musical","Rock & Roll","Hard Rock","Folk","Folk-Rock",
+  "National Folk","Swing","Fast Fusion","Bebob","Latin","Revival",
+  "Celtic","Bluegrass","Avantgarde","Gothic Rock","Progressive Rock",
+  "Psychedelic Rock","Symphonic Rock","Slow Rock","Big Band",
+  "Chorus","Easy Listening","Acoustic","Humour","Speech","Chanson",
+  "Opera","Chamber Music","Sonata","Symphony","Booty Bass","Primus",
+  "Porn Groove","Satire","Slow Jam","Club","Tango","Samba",
+  "Folklore","Ballad","Power Ballad","Rhythmic Soul","Freestyle",
+  "Duet","Punk Rock","Drum Solo","Acapella","Euro-House","Dance Hall"
 ];
 
 module.exports = ID3v1TagReader;

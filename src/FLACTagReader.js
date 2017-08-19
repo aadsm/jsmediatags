27e9e0098896c7cd3b6d688a6894e2801420c3fb
Michael Nguyen <tehtotalpwnage@gmail.com>
2017-08-18 23:29:43 -0700

    Clean up variable naming and add more comments
    Some of the variables were named in ways that were difficult to
    understand, so a couple have been renamed to make their purpose a little
    bit more clear.
    
    In addition, a lot of new comment blocks have been added, which should
    hopefully explain more of the FLAC specification and the purposes of all
    the different numbers.

--- src/FLACTagReader.js
+++ src/FLACTagReader.js
@@ -1,7 +1,27 @@
 var MediaTagReader = require('./MediaTagReader');
 
+/* The first 4 bytes of a FLAC file describes the header for the file. If these
+ * bytes respectively read "fLaC", we can determine it is a FLAC file.
+ */
 const FLAC_HEADER_SIZE = 4;
 
+/* FLAC metadata is stored in blocks containing data ranging from STREAMINFO to
+ * VORBIS_COMMENT, which is what we want to work with.
+ *
+ * Each metadata header is 4 bytes long, with the first byte determining whether
+ * it is the last metadata block before the audio data and what the block type is.
+ * This first byte can further be split into 8 bits, with the first bit being the
+ * last-metadata-block flag, and the last three bits being the block type.
+ *
+ * Since the specification states that the decimal value for a VORBIS_COMMENT block
+ * type is 4, the two possibilities for the comment block header values are:
+ * - 00000100 (Not a last metadata comment block, value of 4)
+ * - 10000100 (A last metadata comment block, value of 132)
+ *
+ * All values for METADATA_BLOCK_HEADER can be found here.
+ * https://xiph.org/flac/format.html#metadata_block_header
+ */
+const COMMENT_HEADERS = [4, 132];
 
 
 /**
@@ -40,6 +60,17 @@ class FLACTagReader extends MediaTagReader {
     return id === 'fLaC';
   }
 
+  /**
+   * Function called to load the data from the file.
+   *
+   * To begin processing the blocks, the next 4 bytes after the initial 4 bytes
+   * (bytes 4 through 7) are loaded. From there, the rest of the loading process
+   * is passed on to the _loadBlock function, which will handle the rest of the
+   * parsing for the metadata blocks.
+   *
+   * @param {MediaFileReader} mediaFileReader - The MediaFileReader used to parse the file.
+   * @param {LoadCallbackType} callbacks - The callback to call once _loadData is completed.
+   */
   _loadData(mediaFileReader: MediaFileReader, callbacks: LoadCallbackType) {
     var self = this;
     mediaFileReader.loadRange([4, 7], {
@@ -71,15 +102,37 @@ class FLACTagReader extends MediaTagReader {
     callbacks: LoadCallbackType
   ) {
     var self = this;
+    /* As mentioned above, this first byte is loaded to see what metadata type
+     * this block represents.
+     */
     var blockHeader = mediaFileReader.getByteAt(offset);
+    /* The last three bytes (integer 24) contain a value representing the length
+     * of the following metadata block. The 1 is added in order to shift the offset
+     * by one to get the last three bytes in the block header.
+     */
     var blockSize = mediaFileReader.getInteger24At(offset + 1, true);
-    if (blockHeader !== 4 && blockHeader !== 132) {
-      mediaFileReader.loadRange([offset + 4 + blockSize, offset + 3 + 4 + blockSize], {
+    /* This conditional checks if blockHeader (the byte retrieved representing the
+     * type of the header) is not the header we are looking for.
+     *
+     * If that is not true, the block is skipped over and the next range is loaded:
+     * - offset + 4 + blockSize adds 4 to skip over the initial metadata header and
+     * blockSize to skip over the block overall, placing it at the head of the next
+     * metadata header.
+     * - offset + 4 + 4 + blockSize does the same thing as the previous block with
+     * the exception of adding another 4 bytes to move it to the end of the new metadata
+     * header.
+     */
+    if (COMMENT_HEADERS.indexOf(blockHeader) === -1) {
+      mediaFileReader.loadRange([offset + 4 + blockSize, offset + 4 + 4 + blockSize], {
         onSuccess: function() {
           self._loadBlock(mediaFileReader, offset + 4 + blockSize, callbacks);
         }
       });
     } else {
+      /* 4 is added to offset to move it to the head of the actual metadata.
+       * The range starting from offsetMatadata (the beginning of the block)
+       * and offsetMetadata + blockSize (the end of the block) is loaded.
+       */
       var offsetMetadata = offset + 4;
       mediaFileReader.loadRange([offsetMetadata, offsetMetadata + blockSize], {
         onSuccess: function() {
@@ -92,34 +145,68 @@ class FLACTagReader extends MediaTagReader {
 
   /**
    * Parses the data and returns the tags.
+   *
+   * This is an overview of the VorbisComment format and what this function attempts to
+   * retrieve:
+   * - First 4 bytes: a long that contains the length of the vendor string.
+   * - Next n bytes: the vendor string encoded in UTF-8.
+   * - Next 4 bytes: a long representing how many comments are in this block
+   * For each comment that exists:
+   * - First 4 bytes: a long representing the length of the comment
+   * - Next n bytes: the comment encoded in UTF-8.
+   * The comment string will usually appear in a format similar to:
+   * ARTIST=me
+   *
+   * Note that the longs and integers in this block are encoded in little endian
+   * as opposed to big endian for the rest of the FLAC spec.
    * @param {MediaFileReader} data - The MediaFileReader to parse the file with.
    * @param {Array<string>} [tags] - Optional tags to also be retrieved from the file.
    * @return {TagType} - An object containing the tag information for the file.
    */
   _parseData(data: MediaFileReader, tags: ?Array<string>): TagType {
-    var string = data.getLongAt(this._offset, false);
+    var vendorLength = data.getLongAt(this._offset, false);
     var offsetVendor = this._offset + 4;
-    var vendor = data.getStringWithCharsetAt(offsetVendor, string, "utf-8").toString();
-    var offsetList = offsetVendor + string;
-    var length = data.getLongAt(offsetList, false);
+    /* This line is able to retrieve the vendor string that the VorbisComment block
+     * contains. However, it is not part of the tags that JSMediaTags normally retrieves,
+     * and is therefore commented out.
+     */
+    // var vendor = data.getStringWithCharsetAt(offsetVendor, vendorLength, "utf-8").toString();
+    var offsetList = vendorLength + offsetVendor;
+    /* To get the metadata from the block, we first get the long that contains the
+     * number of actual comment values that are existent within the block.
+     *
+     * As we loop through all of the comment blocks, we get the data length in order to
+     * get the right size string, and then determine which category that string falls under.
+     * The dataOffset variable is constantly updated so that it is at the beginning of the
+     * comment that is currently being parsed.
+     *
+     * Additions of 4 here are used to move the offset past the first 4 bytes which only contain
+     * the length of the comment.
+     */
+    var numComments = data.getLongAt(offsetList, false);
     var dataOffset = offsetList + 4;
     var title, artist, album, track, genre, picture;
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < numComments; i++) {
       let dataLength = data.getLongAt(dataOffset, false);
       let s = data.getStringWithCharsetAt(dataOffset + 4, dataLength, "utf-8").toString();
       let d = s.indexOf("=");
       let split = [s.slice(0, d), s.slice(d + 1)];
       switch (split[0]) {
         case "TITLE":
-          title = split[1]
+          title = split[1];
+          break;
         case "ARTIST":
-          artist = split[1]
+          artist = split[1];
+          break;
         case "ALBUM":
-          album = split[1]
+          album = split[1];
+          break;
         case "TRACKNUMBER":
-          track = split[1]
+          track = split[1];
+          break;
         case "GENRE":
-          genre = split[1]
+          genre = split[1];
+          break;
       }
       dataOffset += 4 + dataLength;
     }
